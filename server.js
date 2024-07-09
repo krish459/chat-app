fd0cfa1dc63afe96b67a8bf5bfd02df41947a7d6
Krish <krishshah235@gmail.com>
2024-07-09 22:08:46 +0530

    server updated

--- server.js
+++ server.js
@@ -1,12 +1,43 @@
-const io = require("socket.io")(3000, {
-  cors: {
-    origin: "http://localhost:5500",
-    methods: ["GET", "POST"],
-  },
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    fs.readFile(path.join(__dirname, "index.html"), (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        res.end("Error loading index.html");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      }
+    });
+  } else if (req.url === "/script.js") {
+    fs.readFile(path.join(__dirname, "script.js"), (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Internal Server Error");
+      } else {
+        res.writeHead(200, { "Content-Type": "application/javascript" });
+        res.end(data);
+      }
+    });
+  } else {
+    res.writeHead(404);
+    res.end("Not Found");
+  }
 });
 
 const users = {};
 
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 io.on("connection", (socket) => {
   socket.on("new-user", (name) => {
     users[socket.id] = name;
@@ -22,4 +53,8 @@ io.on("connection", (socket) => {
     socket.broadcast.emit("user-disconnected", users[socket.id]);
     delete users[socket.id];
   });
+});
+
+server.listen(PORT, () => {
+  console.log(`${PORT}`);
 });
